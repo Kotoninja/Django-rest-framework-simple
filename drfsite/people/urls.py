2244f7b116e7b9c9574a13366905de0d9bc808e5
artem <SS4T46@yandex.ru>
2025-11-03 13:53:46 +0300

    action

--- drfsite/people/urls.py
+++ drfsite/people/urls.py
@@ -2,8 +2,8 @@ from django.urls import path, include
 from people import views
 from rest_framework import routers
 
-router = routers.SimpleRouter()
+router = routers.DefaultRouter()
 router.register(r"people", views.PeopleViewSet)
-
+print(router.urls)
 
 urlpatterns = [path("", include(router.urls))]
--- drfsite/people/views.py
+++ drfsite/people/views.py
@@ -1,12 +1,24 @@
 from rest_framework import generics
-from .models import People
+from .models import People, Category
 from .serializer import PeopleSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.forms import model_to_dict
 from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import viewsets
+from rest_framework.decorators import action
+
 
 class PeopleViewSet(viewsets.ModelViewSet):
     queryset = People.objects.all()
     serializer_class = PeopleSerializer
+
+    @action(methods=["get"], detail=True)
+    def category(self, request, pk=None):
+        cat = Category.objects.get(pk=pk)
+        return Response({"cat": cat.name})
+
+    @action(methods=["get"], detail=False)
+    def categories(self, request):
+        cats = Category.objects.all()
+        return Response({"cats": [c.name for c in cats]})
